5eabf972b5adf71a41ca4990ec8dee50756d460e
Terrence <terrence.liu@alumni.ubc.ca>
2023-06-08 16:45:25 -0700

    wrote assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+// Function Implementation
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅ " + "Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🛑 " + "Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+
+// Test Code
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("Lighthouse", "Lighthouse");
+assertEqual(1, 1);
+assertEqual(1, 2);
