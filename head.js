ee35d65efb80b4dac4ed76b78b378cd8b7e5b8d2
Terrence <terrence.liu@alumni.ubc.ca>
2023-06-08 17:00:17 -0700

    add head.js

---
+++ head.js
@@ -0,0 +1,19 @@
+// Function Implementation
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(array) {
+  return array.length !== 0 ? array[0] : undefined;
+};
+
+
+
+// Test Code
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
