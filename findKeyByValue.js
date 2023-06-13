bab8800d82945eebae1e82a80bfd3562b77b5a94
Terrence <terrence.liu@alumni.ubc.ca>
2023-06-13 14:36:06 -0700

    added findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,46 @@
+// Function Implementation
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else
+  if (Array.isArray(actual) && Array.isArray(expected)) {
+    if (actual.toString() === expected.toString()) {
+      console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+    }
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  let foundKey;
+  Object.keys(object).forEach((keyValue) => {
+    if (value === object[keyValue]) {
+      foundKey = keyValue;
+    }
+  });
+  return foundKey;
+};
+
+// Test Cases
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+
+const roommateChores = {
+  Paul: "Dusting",
+  Alan: "Vacuuming",
+  Andrew:  "Dishes"
+};
+
+assertEqual(findKeyByValue(roommateChores, "Dusting"), "Paul");
+assertEqual(findKeyByValue(roommateChores, "Vacuuming"), "Alan");
+assertEqual(findKeyByValue(roommateChores, "Mopping"), undefined);
