3f85278db2288c9bf3d63427d0a24bd35fbdc28c
Terrence <terrence.liu@alumni.ubc.ca>
2023-06-20 17:27:06 -0700

    refactored middle and added middle test file

--- middle.js
+++ middle.js
@@ -1,40 +1,4 @@
-// Test/Assertion Functions
-
-// Compares two values and logs a message indicating if they are equal or not
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  }  else {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-// Checks if two arrays are perfectly equal
-const eqArrays = function(array1, array2) {
-  // If array lengths are not equal, terminate early and return false
-  if (array1.length !== array2.length) {
-    return false;
-  }
-
-  // Loop through and compare the indices directly to check for a perfect match
-  // returns false if mismatch found
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-
-  return true;
-};
-
-// Compares two arrays and logs a message indicating if they are equal or not
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`✅ Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${array1} !== ${array2}`);
-  }
-};
+// Function Implementation
 
 // Takes an array and returns the middle of the array as an array items if
 // the array if even and 1 if the array is odd - returns nothing if array
@@ -53,12 +17,4 @@ const middle = function(array) {
   return returnArray;
 };
 
-//Test Cases
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
-
-assertArraysEqual(middle([1, 2, 3]), middle([0, 1, 2, 3, 4]));
+module.exports = middle;
---
+++ test/middleTest.js
@@ -0,0 +1,12 @@
+const middle = require('../middle');
+const assertArraysEqual = require('../assertArraysEqual');
+
+//Test Cases
+console.log(middle([1])); // => []
+console.log(middle([1, 2])); // => []
+console.log(middle([1, 2, 3])); // => [2]
+console.log(middle([1, 2, 3, 4, 5])); // => [3]
+console.log(middle([1, 2, 3, 4])); // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+
+assertArraysEqual(middle([1, 2, 3]), middle([0, 1, 2, 3, 4]));
